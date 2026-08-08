815f2cdc7be2289eaf33f5b8b2e31ad10b214046
Sharanya Datta <sharanyadatta.sd@gmail.com>
2026-08-08 20:47:12 +0530

    Update production code by Sharanya

---
+++ cms/my-blog-cms/.next/dev/static/chunks/app/(frontend)/layout.js
@@ -0,0 +1,39 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/(frontend)/layout"],{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22D%3A%5C%5CMBA%5C%5CSIP%5C%5CAmg%20Venture%20Partners%5C%5CFinal%20Code%5C%5CAMG-WebApp-main%20(1)%5C%5CAMG-WebApp-main%5C%5Ccms%5C%5Cmy-blog-cms%5C%5Csrc%5C%5Capp%5C%5C(frontend)%5C%5Cstyles.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22D%3A%5C%5CMBA%5C%5CSIP%5C%5CAmg%20Venture%20Partners%5C%5CFinal%20Code%5C%5CAMG-WebApp-main%20(1)%5C%5CAMG-WebApp-main%5C%5Ccms%5C%5Cmy-blog-cms%5C%5Csrc%5C%5Capp%5C%5C(frontend)%5C%5Cstyles.css%22%2C%22ids%22%3A%5B%5D%7D&server=false! ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/(frontend)/styles.css */ \"(app-pages-browser)/./src/app/(frontend)/styles.css\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyRCUzQSU1QyU1Q01CQSU1QyU1Q1NJUCU1QyU1Q0FtZyUyMFZlbnR1cmUlMjBQYXJ0bmVycyU1QyU1Q0ZpbmFsJTIwQ29kZSU1QyU1Q0FNRy1XZWJBcHAtbWFpbiUyMCgxKSU1QyU1Q0FNRy1XZWJBcHAtbWFpbiU1QyU1Q2NtcyU1QyU1Q215LWJsb2ctY21zJTVDJTVDc3JjJTVDJTVDYXBwJTVDJTVDKGZyb250ZW5kKSU1QyU1Q3N0eWxlcy5jc3MlMjIlMkMlMjJpZHMlMjIlM0ElNUIlNUQlN0Qmc2VydmVyPWZhbHNlISIsIm1hcHBpbmdzIjoiQUFBQSx3TEFBNksiLCJzb3VyY2VzIjpbIiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkQ6XFxcXE1CQVxcXFxTSVBcXFxcQW1nIFZlbnR1cmUgUGFydG5lcnNcXFxcRmluYWwgQ29kZVxcXFxBTUctV2ViQXBwLW1haW4gKDEpXFxcXEFNRy1XZWJBcHAtbWFpblxcXFxjbXNcXFxcbXktYmxvZy1jbXNcXFxcc3JjXFxcXGFwcFxcXFwoZnJvbnRlbmQpXFxcXHN0eWxlcy5jc3NcIik7XG4iXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22D%3A%5C%5CMBA%5C%5CSIP%5C%5CAmg%20Venture%20Partners%5C%5CFinal%20Code%5C%5CAMG-WebApp-main%20(1)%5C%5CAMG-WebApp-main%5C%5Ccms%5C%5Cmy-blog-cms%5C%5Csrc%5C%5Capp%5C%5C(frontend)%5C%5Cstyles.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./src/app/(frontend)/styles.css":
+/*!***************************************!*\
+  !*** ./src/app/(frontend)/styles.css ***!
+  \***************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"e8d352c43592\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9hcHAvKGZyb250ZW5kKS9zdHlsZXMuY3NzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxjQUFjO0FBQzdCLElBQUksSUFBVSxJQUFJLGlCQUFpQiIsInNvdXJjZXMiOlsiRDpcXE1CQVxcU0lQXFxBbWcgVmVudHVyZSBQYXJ0bmVyc1xcRmluYWwgQ29kZVxcQU1HLVdlYkFwcC1tYWluICgxKVxcQU1HLVdlYkFwcC1tYWluXFxjbXNcXG15LWJsb2ctY21zXFxzcmNcXGFwcFxcKGZyb250ZW5kKVxcc3R5bGVzLmNzcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBcImU4ZDM1MmM0MzU5MlwiXG5pZiAobW9kdWxlLmhvdCkgeyBtb2R1bGUuaG90LmFjY2VwdCgpIH1cbiJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/app/(frontend)/styles.css\n"));
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["main-app"], () => (__webpack_exec__("(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22D%3A%5C%5CMBA%5C%5CSIP%5C%5CAmg%20Venture%20Partners%5C%5CFinal%20Code%5C%5CAMG-WebApp-main%20(1)%5C%5CAMG-WebApp-main%5C%5Ccms%5C%5Cmy-blog-cms%5C%5Csrc%5C%5Capp%5C%5C(frontend)%5C%5Cstyles.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
